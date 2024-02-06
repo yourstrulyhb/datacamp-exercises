9dbfb31d6bef24b9679633314f213244d50ccdde
yourstrulyhb <yourstrulyhb@gmail.com>
2024-02-06 21:38:08 +0800

    Added exercises for using nonpublic Class methods

---
+++ software-engineering-principles-in-python/other-exercises/tokenize_class_attributes.py
@@ -0,0 +1,27 @@
+from collections import Counter 
+
+class Document:
+  def __init__(self, text):
+    self.text = text
+    # Tokenize the document with non-public tokenize method
+    self.tokens = self._tokenize()
+    # Perform word count with non-public count_words method
+    self.word_counts = self._count_words()
+
+  def _tokenize(self):
+    return tokenize(self.text)
+	
+  # non-public method to tally document's word counts with Counter
+  def _count_words(self):
+    return Counter(self.tokens)
+
+datacamp_tweets = 'Welcome to Datacamp! Kickstart start you journey with data!' # sample
+
+# create a new document instance from datacamp_tweets
+datacamp_doc = Document(text=datacamp_tweets)
+
+# print the first 5 tokens from datacamp_doc
+print(datacamp_doc.tokens[:5])
+
+# print the top 5 most used words in datacamp_doc
+print(datacamp_doc.word_counts.most_common(5))
